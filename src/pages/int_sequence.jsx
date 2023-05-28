5f254071ed574941b791c06b27acc19ea0bb3259
e-psi-lon <theg41g@gmail.com>
2023-05-28 22:22:49 +0200

    Just a blank page

---
+++ src/pages/int_sequence.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Head from "next/head";
+import { Header, Footer } from "../components/";
+import styles from "../styles/index.module.css";
+
+const IntSequence = () => {
+    return (
+        <>
+            <Head>
+                <title>Portfolio – Accueil</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <style>{`
+                    html {
+                        color:#FFFFFF;
+                        background-color: #48465D;
+                        font-family: monospace;
+                    }
+                    body {
+                        display: flex;
+                        flex-direction: column;
+                        min-height: 100vh;
+                        position: relative;
+                    }
+                    `}</style>
+            </Head>
+            <div className={styles.content}>
+                <Header />
+                <main>
+                </main>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default IntSequence;
