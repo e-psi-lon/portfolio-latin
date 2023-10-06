81455ccc889d632a1017289d89385dc69ee68bf8
e-psi-lon <theg41g@gmail.com>
2023-10-06 20:57:43 +0200

    Oups (bis)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import Head from "next/head";
 import { Header, Footer } from "../components/";
 import styles from "../styles/index.module.css";
-import { useRouter } from "next/router";
+
 
 const Index = () => {
-    const router = useRouter();
     return (
         <>
             <Head>
@@ -30,7 +29,7 @@ const Index = () => {
                 <main>
                     <p className={styles.p}>Ceci est mon portfolio de latin pour mes années de lycée.</p>
                     <p className={styles.p}>Dedans vous trouverez les mots concepts de chaque séquence accompagnés de leurs définitions respectives. De plus, les travaux de fin de séquence se trouvent dans leur section associée. Ensuite, dans les différentes catégories, vous trouverez aussi les différents travaux réalisés lors des séquences (non encore mis en place).</p>
-                    <p className={styles.p}>Pour finir, vous trouverez le compte-rendu de la conférence des Rendez-vous de l'Histoire sur l'École des Chartes <a className={`${styles.toujours} ${styles.a}`} onClick={router.push("/ecole-des-chartes")}> (non encore mis en place non plus) en cliquant ici</a>.</p>
+                    <p className={styles.p}>Pour finir, vous trouverez le compte-rendu de la conférence des Rendez-vous de l'Histoire sur l'École des Chartes <a className={`${styles.toujours} ${styles.a}`} href="/ecole-des-chartes"> (non encore mis en place non plus) en cliquant ici</a>.</p>
                     <p className={styles.p}>Petit bonus : vous pouvez accéder au code source du portfolio en cliquant sur le bouton ci-dessous : </p>
                     <a className={`${styles.github} ${styles.a}`} href="https://github.com/e-psi-lon/portfolio-latin"> <img src="/github.png"  width={100} height={100} alt="github" /></a>
                 </main>
